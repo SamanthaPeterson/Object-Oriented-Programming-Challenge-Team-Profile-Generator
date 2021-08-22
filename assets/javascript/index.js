255e2531ef995c76f9f400b79529bf00afe6dc3a
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-08-22 10:56:18 -0600

    organize file structure

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,5 @@
+//10. Object-Oriented Programming Challenge: Team Profile Generator
+
 // .├──__tests__ / //jest tests
 //     │├──Employee.test.js│├── Engineer.test.js│├── Intern.test.js│└── Manager.test.js├── dist / // rendered output (HTML) and CSS style sheet      
 //     ├──lib / // classes
@@ -5,6 +7,53 @@
 //     ├──.gitignore // indicates which folders and files Git should ignore
 // ├── index.js // runs the application
 // └── package.json
+
+
+// The application must have these classes: Employee, Manager, Engineer, and Intern.The tests
+// for these classes( in the _tests_ directory) must all pass.
+
+// The first class is an Employee parent class with the following properties and methods:
+
+//     name
+
+// id
+
+// email
+
+// getName()
+
+// getId()
+
+// getEmail()
+
+// getRole() // Returns 'Employee'
+
+// The other three classes will extend Employee.
+
+// In addition to Employee 's properties and methods, Manager will also have:
+
+// officeNumber
+
+// getRole() // Overridden to return 'Manager'
+
+// In addition to Employee 's properties and methods, Engineer will also have:
+
+// github // GitHub username
+
+// getGithub()
+
+// getRole() // Overridden to return 'Engineer'
+
+// In addition to Employee 's properties and methods, Intern will also have:
+
+// school
+
+// getSchool()
+
+// getRole() // Overridden to return 'Intern'
+
+// Finally, although it’ s not a requirement, you should consider adding validation to ensure that user input provided is in the proper expected format.
+
 
 
 //10. Object-Oriented Programming Challenge: Team Profile Generator
