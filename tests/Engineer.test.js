820836d9784122f513431b92d2ef54ec3d0d982b
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-10-26 15:34:10 -0600

    engineer test

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -3,21 +3,21 @@ const Engineer = require('../lib/Engineer');
 
 // creating engineer object  
 test('creates an Engineer object', () => {
-    const engineer = new Engineer('Samantha', 222, 'samantha.peterson@gmail.com', 'nicolewallace09');
+    const engineer = new Engineer('Samantha', 222, 'samantha.peterson@gmail.com', 'samanthapeterson222');
 
     expect(engineer.github).toEqual(expect.any(String));
 });
 
 // gets github from getGithub()
 test('gets engineer github value', () => {
-    const engineer = new Engineer('Samantha', 222, 'samantha.peterson@gmail.com', 'nicolewallace09');
+    const engineer = new Engineer('Samantha', 222, 'samantha.peterson@gmail.com', 'samanthapeterson222');
 
     expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
 });
 
 // gets role from getRole() 
 test('gets role of employee', () => {
-    const engineer = new Engineer('Samantha', 222, 'samantha.peterson@gmail.com', 'nicolewallace09');
+    const engineer = new Engineer('Samantha', 222, 'samantha.peterson@gmail.com', 'samanthapeterson222');
 
     expect(engineer.getRole()).toEqual("Engineer");
 });
